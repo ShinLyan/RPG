2a551b21e9ceb2b8ba131a27153fefdc87f2adb5
Vladimir Shin <vashin@edu.hse.ru>
2022-06-03 23:24:54 +0300

    game build (v.1.2)

--- Scripts/Buttons/SceneButton.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-
-# путь к сцене, которая должна открыться при нажатии этой кнопки
-export(String) var scene_to_open 
--- Scripts/Mouse_cursor.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite
-
-
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-
-
-func _process(_delta):
-	self.position = self.get_global_mouse_position()
